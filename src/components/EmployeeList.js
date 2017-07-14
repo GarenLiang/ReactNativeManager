c33b1084ea0e830ce459918d11c3cf16821790d8
GarenLiang <ideabreeze@gmail.com>
2017-07-13 23:48:46 -0700

    data binding

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeeFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
+  }
   render() {
     return (
       <View>
@@ -12,4 +30,4 @@ class EmployeeList extends Component {
     );
   }
 }
-export default EmployeeList;
+export default connect(null, { employeeFetch })(EmployeeList);
