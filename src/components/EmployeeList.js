66c55c1f2a5c24825724a47fdc47a5c12ab9835c
GarenLiang <ideabreeze@gmail.com>
2017-07-14 15:17:17 -0700

    list item

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text} from 'react-native';
+import { ListView } from 'react-native';
 import { employeeFetch } from '../actions';
-
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
@@ -28,15 +28,17 @@ class EmployeeList extends Component {
     this.dataSource = ds.cloneWithRows(employees);
   }
 
-
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
+  }
 
   render() {
     return (
-      <View>
-        <Text>List</Text>
-        <Text>List</Text>
-        <Text>List</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
     );
   }
 }
