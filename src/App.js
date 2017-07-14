1a2a63a98243acfdd98e55b5d911989fc5d37018
Garen Liang <ideabreeze@gmail.com>
2017-07-14 03:10:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import reducers from './reducers';
 import Router from './Router';
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: "AIzaSyCPtMiTJcb99t9nT8NrYfjuB2cqpzYpB9M",
-      authDomain: "manager-27056.firebaseapp.com",
-      databaseURL: "https://manager-27056.firebaseio.com",
-      projectId: "manager-27056",
-      storageBucket: "manager-27056.appspot.com",
-      messagingSenderId: "770292979376"
-    };
+    
     firebase.initializeApp(config);
   }
   render() {
